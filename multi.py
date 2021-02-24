934a34759b1c9772c5f92c96b3d54c68de3666df
veerastudent <veeraveer002@gmail.com>
2021-02-24 10:48:09 +0000

    multi

--- multi.py
+++ multi.py
@@ -1,21 +1,3 @@
-# Developing a Simple Webserver
-## AIM:
-To develop a simple webserver to serve html pages.
-
-## DESIGN STEPS:
-### Step 1: 
-HTML content creation
-### Step 2:
-Design of webserver workflow
-### Step 3:
-Implementation using Python code
-### Step 4:
-Serving the HTML pages.
-### Step 5:
-Testing the webserver
-
-## PROGRAM:
-## multi.py 
 from http.server import HTTPServer,BaseHTTPRequestHandler
 
 Content="""
@@ -59,12 +41,3 @@ server_address=('',80)
 httpd = HTTPServer(server_address,myhandler)
 print("MY WEBSERVER IS RUNNING...")
 httpd.serve_forever()
-
-
-
-## OUTPUT:
-![output](./static/img/v.png)
-
-
-## RESULT:
-Thus a website is designed for the chip manufacturing company and is hosted in the URL http://veeramalai.student.saveetha.in. HTML code is validated.
